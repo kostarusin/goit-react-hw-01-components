f28522c2201e2bab80dfb7ecde05831896eb9480
Kostyantyn <kosta.rusin@gmail.com>
2023-07-16 23:32:02 +0200

    task 1 full markup without css

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,8 @@
+import style from './FriendList.module.css'
+
+export const FriendList = () => {
+    return <>
+
+    </>
+};
+
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,46 @@
+import PropTypes from "prop-types";
+import style from './Profile.module.css'
+
+
+export const Profile = ({ username, tag, location, avatar, stats }) => {
+
+    return <>
+    <div className={style.profile}>
+  <div className={style.description}>
+    <img
+      src={avatar}
+      alt={username}
+      className={style.avatar}
+    />
+    <p className={style.name}>{username}</p>
+    <p className={style.tag}>@{tag}</p>
+    <p className={style.location}>{location}</p>
+  </div>
+
+  <ul className={style.stats}>
+    <li>
+      <span className={style.label}>Followers</span>
+      <span className={style.quantity}>{stats.followers}</span>
+    </li>
+    <li>
+      <span className={style.label}>Views</span>
+      <span className={style.quantity}>{stats.views}</span>
+    </li>
+    <li>
+      <span className={style.label}>Likes</span>
+      <span className={style.quantity}>{stats.likes}</span>
+    </li>
+  </ul>
+</div>
+ </>
+};
+
+Profile.propTypes = {
+    username: PropTypes.string.isRequired,
+    tag: PropTypes.string.isRequired,
+    location: PropTypes.string.isRequired,
+    avatar: PropTypes.string.isRequired,
+    stats: PropTypes.objectOf(PropTypes.number).isRequired,
+    };
+
+
