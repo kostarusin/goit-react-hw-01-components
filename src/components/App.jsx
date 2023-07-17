0343d5917a3193676ac3a633d2bb7069722ce765
Kostyantyn <kosta.rusin@gmail.com>
2023-07-17 15:15:35 +0200

    statistic is ready instead of css

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,19 @@
-import { Profile }  from './Profile/Profile';
-import user from './Profile/user.json'
+import { Profile } from './Profile/Profile';
+import { Statistic } from './Statistic/Statistic';
+import user from 'components/user.json';
+import data from 'components/data.json';
 
 export const App = () => {
-  return <>
-    <Profile 
-    username={user.username}
-    tag={user.tag}
-    location={user.location}
-    avatar={user.avatar}
-    stats={user.stats}
-    />
-    </>;
+  return (
+    <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistic title="Upload stats" stats={data} />
+    </>
+  );
 };
---
+++ src/components/Statistic/Statistic.jsx
@@ -0,0 +1,31 @@
+import PropTypes from 'prop-types';
+import style from './Statistic.module.css';
+
+export const Statistic = ({ title, stats }) => {
+  return (
+    <>
+      <section className={style.statistics}>
+        {title ? <h2 className={style.title}>{title}</h2> : null}
+        <ul className={style.statList}>
+          {stats.map(stat => (
+            <li className={style.item} key={stat.id}>
+              <span className={style.label}>{stat.label}</span>
+              <span className={style.percentage}>{stat.percentage}</span>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </>
+  );
+};
+
+Statistic.propTypes = {
+    title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+};
